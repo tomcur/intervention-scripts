b3eead56b6465c20aecf30d18eba91d1e340ae78
Thomas Churchman <thomas@kepow.org>
2020-10-23 17:04:51 +0300

    Make loop actually work in bash

--- parallel-collect-teacher-examples.sh
+++ parallel-collect-teacher-examples.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-EPISODES_PER_JOB=10
+EPISODES_PER_JOB=5
 JOBS=40
 DATA_DIRECTORY=/gpfs/hpc/projects/Bolt/end-to-end/carla/datasets/$(date --iso-8601=seconds)-teacher-examples
 
-for j in {1..$JOBS}; do
+job=1
+while [ $job -le $JOBS ]; do
     NUMBER_OF_EPISODES=$EPISODES_PER_JOB \
     INTERVENTION_DATASET_DIRECTORY=$DATA_DIRECTORY \
         sbatch ./job-collect-teacher-examples.sh
+    job=$(($job + 1))
 done
