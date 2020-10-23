16a5d76bf035e5dd95ea47d1a93cebe5158df91b
Thomas Churchman <thomas@kepow.org>
2020-10-23 15:52:54 +0300

    Calculate and compare hash to short-circuit rsync
    rsync on identical Carla directories takes a few minutes, checking the
    hash takes a handful of seconds.

--- prepare-carla-env.sh
+++ prepare-carla-env.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+. ./utils.sh
+
 export XDG_RUNTIME_DIR=/tmp
 export DISPLAY=
 export INTERVENTION_CARLA_SOURCE_DIRECTORY=~/carla/CARLA_0.9.10.1_RSS
 export INTERVENTION_CARLA_DIRECTORY=/tmp/CARLA_0.9.10.1_RSS-7a916435-0cf2-4e4a-a595-6bf6a6e0cb90
 
-echo >&2 "Copying Carla simulator from ${INTERVENTION_CARLA_SOURCE_DIRECTORY} to cluster-local filesystem ${INTERVENTION_CARLA_DIRECTORY}"
-rsync -r "${INTERVENTION_CARLA_SOURCE_DIRECTORY}/" "${INTERVENTION_CARLA_DIRECTORY}"
+CARLA_HASH="9bcfd1021e04366fd3356588ee3b09213069cb888e7ef4c574e406e0cdd9c16e"
+
+echo >&2 "Checking Carla simulator checksum"
+hash=$(tree_sha256_hash "${INTERVENTION_CARLA_DIRECTORY}")
+
+if [ $? -eq 0 ] && [ "${hash}" == "${CARLA_HASH}" ]; then
+    echo >&2 "Carla simulator checksum matches"
+else
+    echo >&2 "Carla simulator checksum does not match."
+    echo >&2 "    Expected: ${CARLA_HASH}"
+    echo >&2 "    Got:      ${hash}"
+    echo >&2 "Copying Carla simulator from ${INTERVENTION_CARLA_SOURCE_DIRECTORY} to cluster-local filesystem ${INTERVENTION_CARLA_DIRECTORY}"
+    rsync -r "${INTERVENTION_CARLA_SOURCE_DIRECTORY}/" "${INTERVENTION_CARLA_DIRECTORY}"
+fi
