cea7621d51678d35fbcaa77e0fefe40d295c67b0
Thomas Churchman <thomas@kepow.org>
2020-10-27 11:14:15 +0200

    Set LD_LIBRARY_PATH to find correct libstdc++

--- job-train-imitation.sh
+++ job-train-imitation.sh
@@ -11,7 +11,8 @@
 
 . ./prepare-intervention-env.sh
 
-intervention-learning train-imitation \
+LD_LIBRARY_PATH="${CONDA_PREFIX}/lib" \
+    intervention-learning train-imitation \
     -d /gpfs/hpc/projects/Bolt/end-to-end/carla/datasets/2020-10-23T17:05:39+0300-teacher-examples \
     -o /gpfs/hpc/projects/Bolt/end-to-end/carla/checkpoints/200-10-26-imitation-2 \
     --num-epochs 15
