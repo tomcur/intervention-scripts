63c112ca5b6c2c5c17112b546aff3e27b5a00f3f
Thomas Churchman <thomas@kepow.org>
2021-10-02 02:35:35 +0300

    Simplify paths, make other options explicit

--- neuron/config.py
+++ neuron/config.py
@@ -6,10 +6,10 @@ CUDA_DEVICES = [0]  # , 1, 2, 3]
 PROCESSES_PER_CUDA_DEVICE = 4
 
 INTERVENTION_CARLA_DIRECTORY = (
-    Path.home() / "code/autonomous-driving/carla/CARLA_0.9.10.1_RSS"
+    Path.home() / "carla/CARLA_0.9.10.1_RSS"
 )
 INTERVENTION_LBC_BIRDVIEW_CHECKPOINT = (
-    Path.home() / "intervention-models/lbc-birdview/model-128.th"
+    Path.home() / "checkpoints/lbc-birdview/model-128.th"
 )
 OUT_DATA_PATH = Path.home() / "datasets"
 TEMPORARY_DIRECTORY = Path.home() / "datasets" / "temp"
@@ -23,8 +23,11 @@ TOWNS = ["Town01", "Town02"]
 WEATHERS = ["ClearNoon"]
 
 #: One of "teacher", "student", "intervention"
+# COLLECT_TYPE = "student"
+# COLLECT_TYPE = "teacher"
 COLLECT_TYPE = "intervention"
 
+# INTERVENTION_TYPE = "regular"
 INTERVENTION_TYPE = "only-student"
 
 #: List of tuples of checkpoint directories and lists of checkpoint numbers to
